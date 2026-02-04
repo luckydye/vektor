bdc4e30f98f7c49fbc12c4a24c6d508364ae9907
lkydy <tim.h4vlicek@gmail.com>
2026-02-04 15:32:52 +0100

    runtime config

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,40 +3,40 @@ export function config() {
     /**
      * Public origin as in the browser
      */
-    SITE_URL: import.meta.env.WIKI_SITE_URL,
+    SITE_URL: process.env.WIKI_SITE_URL,
     /**
      * API host origin (usually same as site_url)
      */
-    API_URL: import.meta.env.WIKI_API_URL,
+    API_URL: process.env.WIKI_API_URL,
     /**
      * Host origin for sync server
      */
-    COLLABORATION_HOST: import.meta.env.WIKI_COLLABORATION_HOST,
+    COLLABORATION_HOST: process.env.WIKI_COLLABORATION_HOST,
 
     /**
      * The default space to redirect to from root "/"
      */
-    DEFAULT_SPACE: import.meta.env.WIKI_DEFAULT_SPACE,
+    DEFAULT_SPACE: process.env.WIKI_DEFAULT_SPACE,
 
     /**
      * better-auth secret token
      */
-    AUTH_SECRET: import.meta.env.AUTH_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
 
     /**
      * OAuth configuration
      */
-    OAUTH_PROVIDER_ID: import.meta.env.OAUTH_PROVIDER_ID,
-    OAUTH_CLIENT_ID: import.meta.env.OAUTH_CLIENT_ID,
-    OAUTH_CLIENT_SECRET: import.meta.env.OAUTH_CLIENT_SECRET,
-    OAUTH_SCOPES: import.meta.env.OAUTH_SCOPES,
-    OAUTH_AUTHORIZATION_URL: import.meta.env.OAUTH_AUTHORIZATION_URL,
-    OAUTH_TOKEN_URL: import.meta.env.OAUTH_TOKEN_URL,
-    OAUTH_USERINFO_URL: import.meta.env.OAUTH_USERINFO_URL,
-    OAUTH_REDIRECT_URI: import.meta.env.OAUTH_REDIRECT_URI,
+    OAUTH_PROVIDER_ID: process.env.OAUTH_PROVIDER_ID,
+    OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
+    OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+    OAUTH_SCOPES: process.env.OAUTH_SCOPES,
+    OAUTH_AUTHORIZATION_URL: process.env.OAUTH_AUTHORIZATION_URL,
+    OAUTH_TOKEN_URL: process.env.OAUTH_TOKEN_URL,
+    OAUTH_USERINFO_URL: process.env.OAUTH_USERINFO_URL,
+    OAUTH_REDIRECT_URI: process.env.OAUTH_REDIRECT_URI,
 
     // Feature flags
-    FEATURE_CANVAS: import.meta.env.WIKI_FEATURE_CANVAS,
+    FEATURE_CANVAS: process.env.WIKI_FEATURE_CANVAS,
   } as const;
 }
 
