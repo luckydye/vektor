b5799be7f13284323991f293e5d47c7f203dc8e2
lkydy <tim.h4vlicek@gmail.com>
2026-06-20 20:04:43 +0200

    fix header image layout shift

--- app/src/components/HeaderImage.vue
+++ app/src/components/HeaderImage.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useDocument } from "../composeables/useDocument.ts";
 import { withTransformParams } from "../files/transformUrl.ts";
 
 const props = defineProps<{ documentId: string }>();
 
-const { document } = useDocument(props.documentId);
+const { document, isLoading } = useDocument(props.documentId);
 
 const src = computed(() => {
   const url = document.value?.properties?.headerImage;
@@ -14,7 +14,13 @@ const src = computed(() => {
 </script>
 
 <template>
-  <div v-if="src" class="print:px-0 px-xs lg:px-xl mt-4">
-    <img :src="src" alt="" class="w-full h-[240px] object-cover rounded-lg" />
+  <div class="print:px-0 px-xs lg:px-xl mt-4">
+    <div v-if="!src || isLoading" class="w-full h-[240px] rounded-lg animate-pulse bg-neutral-50" />
+    <img
+      v-if="src"
+      :src="src"
+      alt=""
+      class="w-full h-[240px] object-cover rounded-lg"
+    />
   </div>
 </template>
