cb74639cbc359fecb58850e975bd918fc9d917ef
lkydy <tim.h4vlicek@gmail.com>
2026-06-25 11:52:39 +0200

    consolidate table extension

--- app/src/editor/extensions.ts
+++ app/src/editor/extensions.ts
@@ -1,5 +1,4 @@
 import type { AnyExtension, Extensions } from "@tiptap/core";
-import { Table, TableCell, TableHeader, TableRow } from "@tiptap/extension-table";
 import {
   BackgroundColor,
   Bold,
@@ -28,7 +27,6 @@ import {
 import { ColumnItem, ColumnLayout } from "./extensions/ColumnLayout.ts";
 import { CommentAnchor } from "./extensions/CommentAnchor.ts";
 import { DatePicker } from "./extensions/DatePicker.ts";
-import { ExpressionCell } from "./extensions/ExpressionCell.ts";
 import { ExtensionView } from "./extensions/ExtensionView.ts";
 import { FigmaEmbed } from "./extensions/FigmaEmbed.ts";
 import { FileAttachment } from "./extensions/FileAttachment.ts";
@@ -37,28 +35,12 @@ import { ImageUpload } from "./extensions/ImageUpload.ts";
 import { MarkdownPaste } from "./extensions/MarkdownPaste.ts";
 import { Mentions } from "./extensions/Mentions.ts";
 import { TicketLink } from "./extensions/TicketLink.ts";
+import { TableEditing } from "./extensions/table.ts";
 import { VideoUpload } from "./extensions/VideoUpload.ts";
 
 export type EditorContext = {
   spaceId?: string;
   documentId?: string;
-};
-
-const colwidthAttribute = {
-  default: [200],
-  parseHTML: (element: HTMLElement) => {
-    const colwidth = element.getAttribute("colwidth");
-    return colwidth ? colwidth.split(",").map((w) => parseInt(w, 10)) : [200];
-  },
-  renderHTML: (attributes: { colwidth?: number[] }) => {
-    if (!attributes.colwidth) {
-      return { style: "width: 200px" };
-    }
-    return {
-      colwidth: attributes.colwidth.join(","),
-      style: `width: ${attributes.colwidth[0]}px`,
-    };
-  },
 };
 
 function withoutDefaultKeyboardShortcuts<T extends AnyExtension>(extension: T): T {
@@ -122,44 +104,7 @@ export function contentExtensions(context: EditorContext = {}): Extensions {
         documentId: documentId,
       }),
     ),
-    withoutDefaultKeyboardShortcuts(
-      Table.configure({
-        resizable: true,
-      }),
-    ),
-    withoutDefaultKeyboardShortcuts(TableRow),
-    withoutDefaultKeyboardShortcuts(
-      TableHeader.extend({
-        addAttributes() {
-          return {
-            ...this.parent?.(),
-            colwidth: colwidthAttribute,
-          };
-        },
-      }),
-    ),
-    withoutDefaultKeyboardShortcuts(
-      TableCell.extend({
-        addAttributes() {
-          return {
-            ...this.parent?.(),
-            colwidth: colwidthAttribute,
-            backgroundColor: {
-              default: null,
-              parseHTML: (element) => element.style.backgroundColor || null,
-              renderHTML: (attributes) => {
-                if (!attributes.backgroundColor) {
-                  return {};
-                }
-                return {
-                  style: `background-color: ${attributes.backgroundColor}`,
-                };
-              },
-            },
-          };
-        },
-      }),
-    ),
+    TableEditing,
     TaskItem.configure({ nested: true }).extend({
       addKeyboardShortcuts() {
         return {
@@ -176,7 +121,6 @@ export function contentExtensions(context: EditorContext = {}): Extensions {
     ExtensionView,
     MarkdownPaste,
     TicketLink,
-    ExpressionCell,
     ColumnLayout,
     ColumnItem,
     HtmlBlock,
---
+++ app/src/editor/extensions/table.ts
@@ -0,0 +1,74 @@
+import { type AnyExtension, Extension } from "@tiptap/core";
+import { Table, TableCell, TableHeader, TableRow } from "@tiptap/extension-table";
+import { ExpressionCell } from "./ExpressionCell.ts";
+
+const colwidthAttribute = {
+  default: [200],
+  parseHTML: (element: HTMLElement) => {
+    const colwidth = element.getAttribute("colwidth");
+    return colwidth ? colwidth.split(",").map((w) => parseInt(w, 10)) : [200];
+  },
+  renderHTML: (attributes: { colwidth?: number[] }) => {
+    if (!attributes.colwidth) {
+      return { style: "width: 200px" };
+    }
+    return {
+      colwidth: attributes.colwidth.join(","),
+      style: `width: ${attributes.colwidth[0]}px`,
+    };
+  },
+};
+
+function withoutDefaultKeyboardShortcuts<T extends AnyExtension>(extension: T): T {
+  return extension.extend({
+    addKeyboardShortcuts: () => ({}),
+  }) as T;
+}
+
+export const TableEditing = Extension.create({
+  name: "tableEditing",
+
+  addExtensions() {
+    return [
+      withoutDefaultKeyboardShortcuts(
+        Table.configure({
+          resizable: true,
+        }),
+      ),
+      withoutDefaultKeyboardShortcuts(TableRow),
+      withoutDefaultKeyboardShortcuts(
+        TableHeader.extend({
+          addAttributes() {
+            return {
+              ...this.parent?.(),
+              colwidth: colwidthAttribute,
+            };
+          },
+        }),
+      ),
+      withoutDefaultKeyboardShortcuts(
+        TableCell.extend({
+          addAttributes() {
+            return {
+              ...this.parent?.(),
+              colwidth: colwidthAttribute,
+              backgroundColor: {
+                default: null,
+                parseHTML: (element) => element.style.backgroundColor || null,
+                renderHTML: (attributes) => {
+                  if (!attributes.backgroundColor) {
+                    return {};
+                  }
+                  return {
+                    style: `background-color: ${attributes.backgroundColor}`,
+                  };
+                },
+              },
+            };
+          },
+        }),
+      ),
+      ExpressionCell,
+    ];
+  },
+});
