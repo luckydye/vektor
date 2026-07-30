ff84ebec35b231a284944f623790c9cdfadae3ae
lkydy <tim.h4vlicek@gmail.com>
2026-07-30 14:47:46 +0200

    fix: Remove white border on avatars

--- app/src/components/AvatarElement.ts
+++ app/src/components/AvatarElement.ts
@@ -115,7 +115,7 @@ function composeAvatar(eyes: string, mouth: string): string {
     ...new Set([...extractFeaturePaths(eyes), ...extractFeaturePaths(mouth)]),
   ];
 
-  return `<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="17" cy="17" r="16.5" stroke="white"/>${uniquePaths.join("")}</svg>`;
+  return `<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">${uniquePaths.join("")}</svg>`;
 }
 
 function getGeneratedAvatar(seed: string): { color: string; src: string } {
